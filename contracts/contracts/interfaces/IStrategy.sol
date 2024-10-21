cde3e6c7ef6f29183e54e020d2c3f0e6d57f2cd0
Richard Jamieson <jamieson.rich@gmail.com>
2024-10-21 15:55:09 +0200

    Change to fractional withdrawal

--- contracts/contracts/interfaces/IStrategy.sol
+++ contracts/contracts/interfaces/IStrategy.sol
@@ -4,7 +4,10 @@ pragma solidity 0.8.26;
 interface IStrategy {
     function invest(uint256 amount) external;
 
-    function withdraw(uint256 _amount) external;
+    function withdraw(
+        uint256 _amount,
+        uint256 _fraction
+    ) external returns (uint256);
 
     function totalUnderlyingAssets() external view returns (uint256);
 }
