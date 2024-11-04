9a895b003b4d34f75a7b5c5581d17fa7f3c9ffcd
Richard Jamieson <jamieson.rich@gmail.com>
2024-11-04 12:40:07 +0200

    Fix: Specify msg.value in invest function

--- contracts/contracts/BaseSepAaveEthStrategy.sol
+++ contracts/contracts/BaseSepAaveEthStrategy.sol
@@ -54,7 +54,11 @@ contract BaseSepAaveEthStrategy is Ownable {
     ) external payable onlyGateway returns (uint256) {
         // note that the amount input here doesn't get used? maybe just check it against msg.value?
         require(msg.value > 0, "No ETH sent");
-        tokenGateway.depositETH(address(aavePool), address(this), 0);
+        tokenGateway.depositETH{value: msg.value}(
+            address(aavePool),
+            address(this),
+            0
+        );
         return msg.value;
     }
 
