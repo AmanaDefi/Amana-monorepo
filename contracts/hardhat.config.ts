6cdddef7ec5c8e46b7fa57acacfb187c55161a1a
Richard Jamieson <jamieson.rich@gmail.com>
2024-11-26 16:53:52 +0200

    Update block number for forked test

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -36,7 +36,7 @@ const config: HardhatUserConfig = {
       chainId: 7001,  // Set the chain ID for your forked network, for example, the ZetaChain testnet chain ID.
       forking: {
         url: `https://zetachain-testnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
-        blockNumber: 7789000 // 20113140, //  
+        blockNumber: 7830907 // 20113140, //  
       },
       allowUnlimitedContractSize: true,
     },
