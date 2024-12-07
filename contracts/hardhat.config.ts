c61b3eadc5a2dc9eb6fe720781b52bb917dba654
Richard Jamieson <jamieson.rich@gmail.com>
2024-12-07 18:46:15 +0200

    Get vault working with new tx sequencing

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -43,22 +43,22 @@ const config: HardhatUserConfig = {
       url: "https://data-seed-prebsc-1-s1.binance.org:8545/",
       accounts: [process.env.PRIVATE_KEY],
     },
-    // hardhat: {
-    //   chainId: 84532,
-    //   forking: {
-    //     url: `https://base-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
-    //     blockNumber: 18675138,
-    //   },
-    //   allowUnlimitedContractSize: true,
-    // },
     hardhat: {
-      chainId: 7001,  // Set the chain ID for your forked network, for example, the ZetaChain testnet chain ID.
+      chainId: 84532,
       forking: {
-        url: `https://zetachain-testnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
-        blockNumber: 8007709
+        url: `https://base-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+        blockNumber: 18675138,
       },
       allowUnlimitedContractSize: true,
     },
+    // hardhat: {
+    //   chainId: 7001,  // Set the chain ID for your forked network, for example, the ZetaChain testnet chain ID.
+    //   forking: {
+    //     url: `https://zetachain-testnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+    //     blockNumber: 8007709
+    //   },
+    //   allowUnlimitedContractSize: true,
+    // },
   },
   solidity: {
     compilers: [
