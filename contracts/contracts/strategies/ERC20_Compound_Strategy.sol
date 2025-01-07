7ee37e2c3d5ae6aef7bf8b20181c7e45dcee4a5c
Richard Jamieson <jamieson.rich@gmail.com>
2025-01-07 11:53:28 +0200

    Create Compound strategy and deploy

---
+++ contracts/contracts/strategies/ERC20_Compound_Strategy.sol
@@ -0,0 +1,105 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "../interfaces/ICompoundVault.sol";
+import "./ERC20StrategyParent.sol";
+
+// BASE_USDC_ADDRESS = 0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913;
+// BASE_COMPOUND_USDC_VAULT_ADDRESS = 0xb125E6687d4313864e53df431d5425969c15Eb2F;
+
+/// @title ERC20_4626_Strategy
+/// @notice Base contract for USDC strategies using Aave and ZetaChain.
+/// @dev Handles USDC investments and divestments for strategies on EVM-compatible chains.
+contract ERC20_Compound_Strategy is ERC20StrategyParent {
+    using SafeERC20 for IERC20;
+
+    ICompoundVault public immutable receiptToken;
+
+    /// @notice Initializes the strategy contract.
+    /// @param _name Name of the strategy.
+    /// @param _amanaVault Address of the Amana vault.
+    /// @param _inputTokenAddress Address of the input token.
+    /// @param _receiptTokenAddress Address of the Aave receipt token.
+    /// @param _gateway Address of the ZetaChain Gateway.
+    constructor(
+        string memory _name,
+        address _amanaVault,
+        address _inputTokenAddress,
+        address _receiptTokenAddress,
+        address _gateway
+    )
+        StrategyParent(_name, _amanaVault, _gateway)
+        ERC20StrategyParent(_inputTokenAddress)
+    {
+        receiptToken = ICompoundVault(_receiptTokenAddress);
+    }
+
+    /// @notice Deposits funds into the yield source.
+    /// @param amount Amount to be deposited.
+    function _depositFundsIntoYieldSource(uint256 amount) internal override {
+        bool success = inputToken.approve(address(receiptToken), amount);
+        if (!success) {
+            revert ApprovalFailed();
+        }
+        receiptToken.supply(address(inputToken), amount);
+        // if (shares == 0) {
+        //     revert DepositFailed();
+        // }
+    }
+
+    /**
+     * @notice Withdraws funds from the configured yield source.
+     * @param amount The amount of funds to withdraw from the yield source.
+     * @return amountWithdrawn The amount of funds successfully withdrawn.
+     */
+    function _withdrawFundsFromYieldSource(
+        uint256 amount
+    ) internal override returns (uint256 amountWithdrawn) {
+        receiptToken.withdrawFrom(
+            address(this),
+            msg.sender,
+            address(inputToken),
+            amount
+        );
+        return amount;
+    }
+
+    /**
+     * @notice Transfers assets from the current strategy to a new strategy.
+     * @dev This function is intended to be overridden in derived contracts to define specific transfer logic.
+     * @param newStrategy The address of the new strategy contract.
+     * @param currentExecutionNonce The current execution nonce for the transaction.
+     * @param _crossChainTxId The cross-chain transaction ID.
+     */
+    function _transferAssetsToNewStrategy(
+        address newStrategy,
+        uint256 currentExecutionNonce,
+        uint256 _crossChainTxId
+    ) internal override {
+        uint256 strategyTotalBalance = receiptToken.balanceOf(address(this)); // TODO use maxwithdraw?
+        _withdrawFundsFromYieldSource(strategyTotalBalance);
+        bool success = inputToken.approve(newStrategy, strategyTotalBalance);
+        if (!success) {
+            revert ApprovalFailed();
+        }
+        IStrategy(newStrategy).depositFromOldStrategy(
+            strategyTotalBalance,
+            currentExecutionNonce,
+            _crossChainTxId
+        );
+        emit AssetsTransferredToNewStrategy(
+            newStrategy,
+            strategyTotalBalance,
+            _crossChainTxId,
+            currentExecutionNonce
+        );
+    }
+
+    /// @notice Gets the total assets held in the strategy.
+    /// @return Total assets as an unsigned integer.
+    function totalUnderlyingAssets() public view override returns (uint256) {
+        return receiptToken.balanceOf(address(this));
+    }
+}
