bae93fc2c5c888cdda5416ce6200590023de1823
Richard Jamieson <jamieson.rich@gmail.com>
2025-01-10 12:46:57 +0200

    Deploy and add new vault to index.ts

--- frontend/src/constants/index.ts
+++ frontend/src/constants/index.ts
@@ -27,7 +27,7 @@ export const tokens: Token[] = [
 
 const MAINNET_VAULT_DATA: VaultData[] = [
   {
-    id: "0x36c7fEdE7556A07AE4f4a4165532f75aa21f2710", // Base ETH Vault
+    id: "0xC4619545d7609BEF7BDC5C438812132Ba837FC38", // Base ETH Vault
     name: "AaveV3 ETH",
     des: " This vault invests ETH into a simple strategy which deposits the funds as collateral into an Aave ETH pool, which earns interest every block.",
     symbol: "aAaveETH",
@@ -43,7 +43,7 @@ const MAINNET_VAULT_DATA: VaultData[] = [
     },
     protocol: {
       name: "Aave",
-      strategyAddress: "0x03E55642EA26d681d6Ddfd818259F8E4986eca69",
+      strategyAddress: "0x74D8De227791a6678720d7f6d77435da9cAB48d6",
       network: "Base",
       chainId: 8453,
       netdes: "Base is a relatively new chain, backed by Coinbase and built on the OP stack.",
