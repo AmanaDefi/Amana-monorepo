51b2ed77ced70a6bb347e41c37d84767582a76f5
Richard Jamieson <jamieson.rich@gmail.com>
2025-01-14 13:13:00 +0200

    Update polygon vault details

--- frontend/src/constants/index.ts
+++ frontend/src/constants/index.ts
@@ -52,7 +52,7 @@ const MAINNET_VAULT_DATA: VaultData[] = [
     },
   },
   {
-    id: "0xA73efAB144Cd771BEF44b2D4f5024790B0EBfD5d", // Base ETH Vault
+    id: "0x3c1E5642F46412797e5e4ACF81DAcAe953e3ab60", // Polygon POL Vault
     name: "AaveV3 POL",
     des: " This vault invests POL into a simple strategy which deposits the funds as collateral into an Aave POL pool, which earns interest every block.",
     symbol: "aAavePOL",
@@ -68,7 +68,7 @@ const MAINNET_VAULT_DATA: VaultData[] = [
     },
     protocol: {
       name: "Aave",
-      strategyAddress: "0x2951CeE73b27c2b1Ffd66A03b77eEdD79012d2BF",
+      strategyAddress: "0xFFcB9E833403c311f99d4f2E32Cdf61d4Eb0695f",
       network: "Polygon",
       chainId: 137,
       netdes: "Polygon is a POS side chain to Ethereum.",
