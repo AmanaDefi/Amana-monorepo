42eb465ec6c784601c3acc8a4cac47a4f61b30fe
Paicu Ionut <paicuionut121698@gmail.com>
2025-01-20 15:20:57 +0200

    fix: changes for vaults/id as well and removed /vaults page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,17 +3,27 @@
 import React from "react";
 import VaultsContainer from "../containers/VaultsContainer";
 import Footer from "../components/Footer";
-import { SUPPORTED_CHAINS } from "../constants/chainConfig";
 import Header from "@/components/header";
+import {useActiveAccount} from "thirdweb/react";
 
 export default function Page() {
+  const account = useActiveAccount();
   return (
-    <main className="p-4 pb-10 min-h-screen flex flex-col container mx-auto relative overflow-hidden">
-      <Header />
-      <div className="flex-1 flex flex-col justify-between py-20 px-6">
-        <VaultsContainer activeChain={SUPPORTED_CHAINS[0]} />
-        <Footer />
-      </div>
-    </main>
+      <main className="p-4 pb-10 min-h-screen flex flex-col container mx-auto relative overflow-hidden">
+          <div className="flex flex-col h-screen">
+              <Header/>
+              {
+                  account &&
+                  <div className="flex-1 flex flex-col justify-between py-20 pl-6">
+                      <div className="flex-1">
+                          <VaultsContainer/>
+                      </div>
+
+                      {/* Footer aligned with the main content */}
+                      <Footer/>
+                  </div>
+              }
+          </div>
+      </main>
   );
 }
--- frontend/src/app/vaults/[id]/page.tsx
+++ frontend/src/app/vaults/[id]/page.tsx
@@ -1,45 +1,35 @@
 "use client";
 
-import React, { useEffect } from "react";
+import React from "react";
 import { useActiveAccount } from "thirdweb/react";
 import VaultsDetailContainer from "@/containers/VaultsDetailContainer";
 import Footer from "../../../components/Footer";
 import Header from "@/components/header";
-import { useRouter, useParams } from 'next/navigation';
+import { useParams } from 'next/navigation';
 
 
 function Index({ }) {
 
   const account = useActiveAccount();
-  const router = useRouter();
   const { id } = useParams();
-  useEffect(() => {
-    if (!account) {
-      router.push("/");
-    }
-  }, [account, router]);
-
 
   return (
-    <>
-      {
-        account &&
-        <main className="p-4 pb-10 min-h-screen flex flex-col container mx-auto relative">
-          <div className="flex flex-col">
-            <Header />
-            <div className="flex-1 flex flex-col justify-between py-20 pl-6">
-              <div className="flex-1">
-                <VaultsDetailContainer vaultID={id} />
+      <main className="p-4 pb-10 min-h-screen flex flex-col container mx-auto relative">
+        <div className="flex flex-col">
+          <Header/>
+          {
+              account &&
+              <div className="flex-1 flex flex-col justify-between py-20 pl-6">
+                <div className="flex-1">
+                  <VaultsDetailContainer vaultID={id}/>
+                </div>
+
+                {/* Footer aligned with the main content */}
+                <Footer/>
               </div>
-
-              {/* Footer aligned with the main content */}
-              <Footer />
-            </div>
-          </div>
-        </main>
-      }
-    </>
-
+          }
+        </div>
+      </main>
   )
 }
 
