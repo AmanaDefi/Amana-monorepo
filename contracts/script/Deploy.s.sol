0fca9d01c8f2413097abfdccc9cdda2a9f8dcfb3
Richard Jamieson <jamieson.rich@gmail.com>
2025-01-23 13:31:09 +0200

    Change salt to avoid address collision

--- contracts/script/Deploy.s.sol
+++ contracts/script/Deploy.s.sol
@@ -22,7 +22,7 @@ contract Deploy is Script {
         vm.startBroadcast(deployerPrivateKey);
 
         // Define a unique salt for deterministic deployment
-        bytes32 salt = keccak256(abi.encodePacked("WithdrawalReceiver12345"));
+        bytes32 salt = keccak256(abi.encodePacked("WithdrawalReceiver1234567"));
 
         // Deploy the WithdrawalReceiver contract using CREATE2
         WithdrawalReceiver withdrawalReceiver = new WithdrawalReceiver{
