a4a2d1f28863900f0fd21c0f3bbd1cf7f2a780ff
Richard Jamieson <jamieson.rich@gmail.com>
2025-01-28 09:20:09 +0200

    Update first vault in mainnet list

--- frontend/src/constants/index.ts
+++ frontend/src/constants/index.ts
@@ -224,7 +224,7 @@ const MAINNET_VAULT_DATA: VaultData[] = [
 
 const TESTNET_VAULT_DATA: VaultData[] = [
   {
-    id: "0xDE99D2a1a5e629DAF6A09d3A00568DD8acA1Ba96", // Base Sepolia ETH Vault
+    id: "0xF1bF99FB21f1381b73d7F610E3054d14cC38Aae2", // Base Sepolia ETH Vault
     name: "AaveV3 ETH",
     des: " This vault invests ETH into a simple strategy which deposits the funds as collateral into an Aave ETH pool, which earns interest every block.",
     symbol: "aAaveETH",
@@ -240,7 +240,7 @@ const TESTNET_VAULT_DATA: VaultData[] = [
     },
     protocol: {
       name: "Aave",
-      strategyAddress: "0xC058E93347162563f53893e1dF20e3cC017C85b3",
+      strategyAddress: "0xEc8461Aa545CAa0e784b395a2569Ca46641ab151",
       chainId: 84532,
       network: "Base Sepolia",
       netdes: "Base is a relatively new chain, backed by Coinbase and built on the OP stack.",
