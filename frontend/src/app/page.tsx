f8b58855f338493507fa6e832a3a6d8b52405660
Paicu Ionut <paicuionut121698@gmail.com>
2025-02-10 12:43:13 +0200

    Add beta warning

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,11 +3,18 @@
 import React from "react";
 import VaultsContainer from "../containers/VaultsContainer";
 import {useActiveAccount} from "thirdweb/react";
+import Link from "next/link";
 
 export default function Page() {
   const account = useActiveAccount();
   return (
-      <>
+      <div className='flex flex-col w-full'>
+          <div className='w-full text-center bg-amber-50'>
+                <span className='py-2 lg:py-4 px-4 text-black text-sm'>
+                    This site is currently in beta and may contain bugs 🐞. Report any bugs or give feedback <Link
+                    href='mailto:info@amanadefi.com' className='underline-offset-2 underline'>here</Link>
+                </span>
+          </div>
           {
               account &&
               <div className="flex-1 flex flex-col w-full justify-between pt-20">
@@ -16,6 +23,6 @@ export default function Page() {
                   </div>
               </div>
           }
-      </>
+      </div>
   );
 }
