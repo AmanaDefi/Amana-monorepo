a7da634bca89463c8c896a5e5791feca8d19a94f
mooncitydev <hellomooncity@gmail.com>
2025-03-31 21:26:17 +0900

    fix:update message background

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,33 +2,31 @@
 
 import React from "react";
 import VaultsContainer from "../containers/VaultsContainer";
-import {useActiveAccount} from "thirdweb/react";
+import { useActiveAccount } from "thirdweb/react";
 import Link from "next/link";
-import {BannersCarousel} from "@/components/banners/BannersCarousel";
+import { BannersCarousel } from "@/components/banners/BannersCarousel";
+import { Note } from "@/components/Note";
 
-const guideText = "Deposit to any of the vaults below from any of our connected chains - Ethereum, Basem Polygon, BNB Smart Chain or Zetachain. Switch active chain using the wallet connection button in the top right hand corner of the screen!"
 
 export default function Page() {
   const account = useActiveAccount();
   return (
-      <div className='flex flex-col w-full'>
-          <div className='w-full text-center bg-amber-50'>
-                <span className='py-2 lg:py-4 px-4 text-black text-sm'>
-                    This site is currently in beta and may contain bugs 🐞. Report any bugs or give feedback <Link
-                    href='mailto:info@amanadefi.com' className='underline-offset-2 underline'>here</Link>
-                </span>
-          </div>
-          <div className='pb-10 pt-5 lg:pb-14 lg:pt-10'>
-              <BannersCarousel />
-          </div>
-          <div>
-            {guideText}
-          </div>
-          <div className="flex-1 flex flex-col w-full justify-between pb-10">
-              <div className="flex-1 p-4 container mx-auto gap-5">
-                  <VaultsContainer/>
-              </div>
-          </div>
+    <div className='flex flex-col w-full'>
+      <div className='w-full text-center bg-amber-50'>
+        <span className='py-2 lg:py-4 px-4 text-black text-sm'>
+          This site is currently in beta and may contain bugs 🐞. Report any bugs or give feedback <Link
+            href='mailto:info@amanadefi.com' className='underline-offset-2 underline'>here</Link>
+        </span>
       </div>
+      <div className='pb-10 pt-5 lg:pb-14 lg:pt-10'>
+        <BannersCarousel />
+      </div>
+      <Note />
+      <div className="flex-1 flex flex-col w-full justify-between pb-10">
+        <div className="flex-1 p-4 container mx-auto gap-5">
+          <VaultsContainer />
+        </div>
+      </div>
+    </div>
   );
 }
