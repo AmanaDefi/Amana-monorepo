893a7bc50cc705e00d1dec00e4a44878f8f95f34
Richard Jamieson <jamieson.rich@gmail.com>
2025-04-02 15:31:20 +0200

    Use upgrade script for old BNB vault

--- contracts/script/UpgradeVault.s.sol
+++ contracts/script/UpgradeVault.s.sol
@@ -2,13 +2,13 @@
 pragma solidity ^0.8.26;
 
 import "forge-std/Script.sol";
-import {AmanaConnectedChainVault} from "../contracts/AmanaConnectedChainVault.sol";
+import {AmanaConnectedChainVaultV1} from "../contracts/AmanaConnectedChainVaultV1.sol";
 import {console} from "forge-std/console.sol";
 
 contract UpgradeVault is Script {
     function run() external {
         // ✅ Hardcoded proxy address to upgrade
-        address proxyAddress = 0x48CAD47d5F0211c6610754F511d3A75eCd7c3084;
+        address proxyAddress = 0xb1d9b596799aC5fe6Ac7510392EC037B5Df04724;
 
         // ✅ Fetch the PRIVATE_KEY from environment and ensure it starts with "0x"
         string memory rawPrivateKey = vm.envString("PRIVATE_KEY");
@@ -26,7 +26,7 @@ contract UpgradeVault is Script {
         vm.startBroadcast(deployerPrivateKey);
 
         // ✅ Deploy the new implementation
-        AmanaConnectedChainVault newImpl = new AmanaConnectedChainVault();
+        AmanaConnectedChainVaultV1 newImpl = new AmanaConnectedChainVaultV1();
         console.log(" New implementation deployed at:", address(newImpl));
 
         // ✅ Upgrade the proxy via `upgradeToAndCall`
