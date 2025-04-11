3caa79f7d9fab117aba0d748d57ed74bd1b9d747
Richard Jamieson <jamieson.rich@gmail.com>
2025-04-11 15:59:14 +0200

    Comment out note for now

--- frontend/src/components/Note.tsx
+++ frontend/src/components/Note.tsx
@@ -8,11 +8,11 @@ export function Note() {
     const handleClose = () => setShow(false);
     return (
         <>
-            {show && <div className="relative text-black w-full text-center bg-amber-50 px-2 py-1 gap-2">
+            {/* {show && <div className="relative text-black w-full text-center bg-amber-50 px-2 py-1 gap-2">
                 <p>Deposit to any of the vaults below from any of our connected chains - Ethereum, Base, Polygon, BNB Smart Chain or Zetachain.</p>
                 <p>Switch active chain using the wallet connection button in the top right hand corner of the screen!</p>
                 <div className="absolute right-2 top-0 cursor-pointer hover:text-red-400" onClick={handleClose}>x</div>
-            </div>}
+            </div>} */}
         </>
     )
 }
