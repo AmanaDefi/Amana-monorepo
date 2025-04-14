7c67a536151556b9bd1aae2f773750d49cdf9892
Quirin NextFlow <info@nextflow-digital.de>
2025-04-14 09:45:58 +0200

    Update Note.tsx

--- frontend/src/components/Note.tsx
+++ frontend/src/components/Note.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { XMarkIcon } from "@heroicons/react/24/solid";
 
-const note = "Deposit to any of the vaults below from any of our connected chains - Ethereum, Base, Polygon, BNB Smart Chain or Zetachain. Switch active chain using the wallet connection button in the top right hand corner of the screen!"
+const note = "Secure your spot on the leaderboard by depositing now. Points are key for your airdrop allocation!"
 
 export function Note() {
     const [show, setShow] = useState<Boolean>(true);
@@ -9,8 +9,7 @@ export function Note() {
     return (
         <>
             {/* {show && <div className="relative text-black w-full text-center bg-amber-50 px-2 py-1 gap-2">
-                <p>Deposit to any of the vaults below from any of our connected chains - Ethereum, Base, Polygon, BNB Smart Chain or Zetachain.</p>
-                <p>Switch active chain using the wallet connection button in the top right hand corner of the screen!</p>
+                <p>Secure your spot on the leaderboard by depositing now. Points are key for your airdrop allocation!</p>
                 <div className="absolute right-2 top-0 cursor-pointer hover:text-red-400" onClick={handleClose}>x</div>
             </div>} */}
         </>
