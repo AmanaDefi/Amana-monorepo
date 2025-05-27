42634246d2666b4c81d09cd6682bfc914e9c6348
olha_sydorchuk <157120293+Oolha@users.noreply.github.com>
2025-05-27 11:37:51 +0200

    fix/AM-9/memoize ApiService instances and fix queryKey in API hooks

--- frontend/src/hooks/useInboundToCctxData.ts
+++ frontend/src/hooks/useInboundToCctxData.ts
@@ -1,13 +1,17 @@
 import { ApiService } from "@/service";
 import { Action } from "@/types/types";
 import { useQuery } from "@tanstack/react-query";
+import { useMemo } from "react";
 
 export const useInboundToCctxData = (crossschainInvestHash: string, action: Action) => {
+  const apiService = useMemo(() => new ApiService(), []); 
+
   const { data, isLoading, error } = useQuery({
-    queryKey: ["InboundToCctxData", crossschainInvestHash],
-    queryFn: () => new ApiService().blockpi.getInboundHashToCctxData(crossschainInvestHash),
+    queryKey: ["InboundToCctxData", crossschainInvestHash, action],
+    queryFn: () =>
+      apiService.blockpi.getInboundHashToCctxData(crossschainInvestHash),
     refetchInterval: 5000,
-    enabled: crossschainInvestHash != "" && action == Action.crosschainInvest
+    enabled: crossschainInvestHash !== "" && action === Action.crosschainInvest,
   });
 
   return data
--- frontend/src/hooks/useLeaderboardData.ts
+++ frontend/src/hooks/useLeaderboardData.ts
@@ -1,12 +1,16 @@
 import { ApiService } from "@/service"
 import { SearchParams } from "@/types/types"
 import { useQuery } from "@tanstack/react-query"
+import { useMemo } from "react";
 
 export const useLeaderboardData = (searchParams: SearchParams): { data: any, isLoading: boolean, error: any } => {
+
+     const apiService = useMemo(() => new ApiService(), []);
+     
     const { data, isLoading, error } = useQuery({
-        queryKey: ["LeaderboardData", searchParams],
-        queryFn: () => new ApiService().getLeaderboardData(searchParams)
-    })
+      queryKey: ["LeaderboardData", searchParams],
+      queryFn: () => apiService.getLeaderboardData(searchParams),
+    });
 
     return { data, isLoading, error }
 }
