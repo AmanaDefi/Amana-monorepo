d7a280636accf37860fbdd1f30e61d8c292ff406
Richard Jamieson <jamieson.rich@gmail.com>
2025-06-03 13:34:42 +0200

    Deploy vault upgrade

--- contracts/script/UpgradeVault.s.sol
+++ contracts/script/UpgradeVault.s.sol
@@ -8,14 +8,14 @@ import {console} from "forge-std/console.sol";
 contract UpgradeVault is Script {
     function run() external {
         // ✅ Hardcoded proxy address to upgrade
-        address proxyAddress = 0xb8F0fBee642638C11019139Dee02D1b18E91b28E;
+        address proxyAddress = 0x19C5C50B3d02f6cDB47c06a12C625ee644bf5B4A;
         // address newImpl = 0x1a4810A0Dc61FF4d3D46Cb8Ac89612Cc286Ca11C; // <- older version with old switchStrategy function
         // address newImpl = 0x198938Cb9429D35562569AC567f063654166c636; // <- final version with updated switchStrategy function and toggleDepositFeePaidFromGasTank
         // address newImpl = 0xcB4b1936df6B44967Ca44A28BbC63cF1e886d06D;
         // address newImpl = 0xBDC27D4Bd051b774fB33AEFae10Bb6D698bAcEA6; // AmanaConnectedChainVault: 0x502881c6f25340e62757a7be556b0e8ccbdb195d
         // address newImpl = 0x6fE22F986797F16aabb461df546ed3317932213f; // V1 vault (old version)
 
-        address newImpl = 0x00Bee4960d02B51841b811a50589a2a6b17B191F; // V1 vault (older version)
+        address newImpl = 0xcd0DcA0347b1a05B98194950683afe112C44DB9f; // V1 vault (older version)
 
         // ✅ Fetch the PRIVATE_KEY from environment and ensure it starts with "0x"
         string memory rawPrivateKey = vm.envString("PRIVATE_KEY");
