3f277d04623d3ba11a5963937b35ea7820852987
olha_sydorchuk <157120293+Oolha@users.noreply.github.com>
2025-06-10 12:31:06 +0200

    fix/AM-84/add deposit transaction ui

--- frontend/src/components/VaultsDetailsWrapper/components/DepositInstruction.tsx
+++ frontend/src/components/VaultsDetailsWrapper/components/DepositInstruction.tsx
@@ -1,13 +1,49 @@
+import ConfirmDepositIcon from "@/components/svg/instruction/ConfirmDepositIcon";
+import CrossChainTransferIcon from "@/components/svg/instruction/CrossChainTransferIcon";
+import FinalConfirmationIcon from "@/components/svg/instruction/FinalConfirmationIcon";
+import SelectTokenIcon from "@/components/svg/instruction/SelectTokenIcon";
 import React from "react";
 
-interface DepositInstructionProps {
- 
-}
+interface DepositInstructionProps {}
 
 const DepositInstruction: React.FC<DepositInstructionProps> = ({}) => {
   return (
-    <div></div>
-  )
+    <div className="flex flex-col gap-[30px]">
+      <div className="flex flex-row gap-4 items-center">
+        <div className="rounded-full w-11 h-11 bg-[#535E73] flex items-center justify-center">
+          <SelectTokenIcon width={20} height={20} />
+        </div>
+        <p className="text-[18px] font-bold tracking-[-0.06em]">
+          Select the token you want to deposit
+        </p>
+      </div>
+      <div className="flex flex-row gap-4 items-center">
+        <div className="rounded-full w-11 h-11 bg-[#535E73] flex items-center justify-center">
+          <ConfirmDepositIcon width={20} height={20} />
+        </div>
+        <p className="text-[18px] font-bold tracking-[-0.06em]">
+          Confirm deposit
+        </p>
+      </div>
+      <div className="flex flex-row gap-4 items-center">
+        <div className="rounded-full w-11 h-11 bg-[#535E73] flex items-center justify-center">
+          <CrossChainTransferIcon width={20} height={20} />
+        </div>
+        <p className="text-[18px] font-bold tracking-[-0.06em]">
+          Cross-chain transfer and investment of funds
+        </p>
+      </div>
+      <div className="flex flex-row gap-4 items-center">
+        <div className="rounded-full w-11 h-11 bg-[#535E73] flex items-center justify-center">
+          <FinalConfirmationIcon width={20} height={20} />
+        </div>
+        <p className="text-[18px] font-bold tracking-[-0.06em]">
+          Final confirmation and issue of shares
+        </p>
+      </div>
+      <div className="rounded-[4px] h-[2px] bg-[#535E73]"></div>
+    </div>
+  );
 };
 
 export default DepositInstruction;
