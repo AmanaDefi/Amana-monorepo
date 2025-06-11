3145c7bec2bea6f39fca42e17d5667a2071abc42
Richard Jamieson <jamieson.rich@gmail.com>
2025-06-11 16:49:48 +0200

    Upgrade SwapHelperZetachain

--- contracts/script/Upgrade.s.sol
+++ contracts/script/Upgrade.s.sol
@@ -7,10 +7,10 @@ import {console} from "forge-std/console.sol";
 contract Upgrade is Script {
     function run() external {
         // 🛰️ Proxy address you want to upgrade
-        address proxyAddress = 0x70f92e46824b2FeaC3EE5f5877dDe3a3F6b17e7a;
+        address proxyAddress = 0x7fA73727f315cFD0d6BdFDD86d165430117FAEBb;
 
         // 🆕 New implementation contract address
-        address newImpl = 0xa37B39eB6451836Cd964cB2FC0f82485BE760831;
+        address newImpl = 0x40c14441aaeBE613c4cF51B274825cBacc7b199e;
 
         // 🔐 Get private key from .env and prefix if needed
         string memory rawPrivateKey = vm.envString("PRIVATE_KEY");
