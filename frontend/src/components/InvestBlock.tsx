ec7855b8dd45c67b5ea7c53fb1315e70d96c59fa
olha_sydorchuk <157120293+Oolha@users.noreply.github.com>
2025-06-11 09:15:36 +0200

    fix/AM-90/changed click on invest button

--- frontend/src/components/InvestBlock.tsx
+++ frontend/src/components/InvestBlock.tsx
@@ -9,6 +9,7 @@ import { SUPPORTED_CHAINS } from "@/constants/chainConfig";
 import { useMultiChain } from "@/providers/MultiChainProvider";
 import { inAppWallet, createWallet } from "thirdweb/wallets";
 import { wallets } from "@/constants/wallets";
+import { useAuthStore } from "@/store/authStore";
 
 const InvestBlock = () => {
   const { walletAddress } = useMultiChain();
@@ -18,6 +19,7 @@ const InvestBlock = () => {
     // Logic for replenishing the wallet
     console.log("Fund wallet clicked");
   };
+  const { openStep } = useAuthStore();
 
   return (
     <div className="font-gotham pl-[44px] pr-[40px] py-[18px] flex items-center justify-between rounded-[16px] bg-[rgba(20,23,31,0.15)] backdrop-blur-[20px] shadow-md before-gradient-border">
@@ -45,16 +47,12 @@ const InvestBlock = () => {
         </Button>
       ) : (
         <div className="thirdweb-invest-button">
-          <ConnectButton
-            wallets={wallets}
-            chains={SUPPORTED_CHAINS}
-            client={client}
-            connectButton={{
-              label: "Invest",
-              className:
-                "w-[192px] h-[56px] text-[14px] font-normal rounded-lg bg-[#1B46E0] !text-white border border-[#323234] shadow-[0_4px_4px_0_rgba(0,0,0,0.15),inset_0_2px_4px_0_#5251c5] backdrop-blur-[20px] hover:backdrop-blur-[20px] hover:shadow-[inset_0_2px_4px_0_#5251c5] active:bg-[#1B46E0] active:backdrop-blur-[20px] active:shadow-[inset_0_2px_4px_0_#5251c5] flex items-center justify-center transition",
-            }}
-          />
+          <button
+            onClick={() => openStep("optionsA")}
+            className="w-[192px] h-[56px] text-[18px] font-normal rounded-lg bg-[#1B46E0] text-white border border-[#323234] shadow-[0_4px_4px_0_rgba(0,0,0,0.15),inset_0_2px_4px_0_#5251c5] backdrop-blur-[20px] hover:backdrop-blur-[20px] hover:shadow-[inset_0_2px_4px_0_#5251c5] active:bg-[#1B46E0] active:backdrop-blur-[20px] active:shadow-[inset_0_2px_4px_0_#5251c5] flex items-center justify-center transition"
+          >
+            Invest
+          </button>
         </div>
       )}
     </div>
