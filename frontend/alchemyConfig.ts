88c041878bb40b0fccd63f6a12ac56fe18db4bc2
VKdrvtsv <valery.volikova1@gmail.com>
2025-06-13 15:18:22 +0300

    add-wallet-providers-and-fix-passkey

--- frontend/alchemyConfig.ts
+++ frontend/alchemyConfig.ts
@@ -6,6 +6,8 @@ import {
 import { alchemy } from "@account-kit/infra";
 import { QueryClient } from "@tanstack/react-query";
 import { SUPPORTED_CHAINS, AlchemyZetachain } from "@/constants/chainConfig";
+import { injected } from "wagmi";
+import { walletConnect } from "wagmi/connectors";
 
 export const alchemyApiKey =
   process.env.NEXT_PUBLIC_ALCHEMY_API_KEY || "WOD1jehYaPtOI8ix7dFiP";
@@ -17,23 +19,23 @@ if (!alchemyApiKey) throw new Error("Invalid Alchemy API KEY");
 if (!walletConnectProjectId) throw new Error("Invalid WalletConnect Id");
 
 const uiConfig: AlchemyAccountsUIConfig = {
-  illustrationStyle: "filled",
-  auth: {
-    sections: [
-      [{ type: "email" }],
-      [
-        { type: "passkey" },
-        { type: "social", authProviderId: "google", mode: "popup" },
-      ],
-      [
-        {
-          type: "external_wallets",
-          walletConnect: { projectId: walletConnectProjectId },
-        },
-      ],
-    ],
-    addPasskeyOnSignup: true,
-  },
+  // illustrationStyle: "filled",
+  // auth: {
+  //   sections: [
+  //     [{ type: "email" }],
+  //     [
+  //       { type: "passkey" },
+  //       { type: "social", authProviderId: "google", mode: "popup" },
+  //     ],
+  //     // [
+  //     //   {
+  //     //     type: "external_wallets",
+  //     //     walletConnect: { projectId: walletConnectProjectId },
+  //     //   },
+  //     // ],
+  //   ],
+  //   addPasskeyOnSignup: true,
+  // },
 };
 
 export const alchemyConfig = createConfig(
@@ -44,6 +46,15 @@ export const alchemyConfig = createConfig(
     ssr: true,
     storage: cookieStorage,
     enablePopupOauth: true,
+    connectors: [
+      walletConnect({
+        projectId: walletConnectProjectId,
+        showQrModal: true,
+        qrModalOptions: {
+          themeMode: "dark",
+        },
+      }),
+    ],
   },
   uiConfig,
 );
--- frontend/src/providers/MultiChainProvider.tsx
+++ frontend/src/providers/MultiChainProvider.tsx
@@ -15,7 +15,6 @@ import { useWallet } from "@solana/wallet-adapter-react";
 import { useWalletModal } from "@solana/wallet-adapter-react-ui";
 import { CHAIN_ID, chainConfigs } from "@/constants/chainConfig";
 import {
-  useAuthModal,
   useLogout,
   useUser,
   useChain,
@@ -47,7 +46,6 @@ interface MultiChainContextType {
   walletAddress: string | null;
   balance: Balance;
   connectSolana: () => Promise<void>;
-  connectEthereum: () => void;
   disconnectWallet: () => void;
   isModalOpen: boolean;
   setIsModalOpen: Dispatch<SetStateAction<boolean>>;
@@ -71,7 +69,6 @@ export const MultiChainProvider = ({ children }: { children: ReactNode }) => {
   const [selectedChain, setSelectedChain] = useState<ChainType | null>(null);
   const [walletAddress, setWalletAddress] = useState<string | null>(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const { openAuthModal, isOpen } = useAuthModal();
   const activeAccount = useUser();
   const { logout: evmDisconnect } = useLogout();
   const { publicKey, disconnect, connected } = useWallet();
@@ -129,14 +126,6 @@ export const MultiChainProvider = ({ children }: { children: ReactNode }) => {
       console.error("Solana connection error:", error);
     }
   }, [selectedChain, activeAccount, evmDisconnect, setVisible]);
-
-  const connectEthereum = useCallback(() => {
-    setIsModalOpen(false);
-
-    if (openAuthModal) {
-      openAuthModal();
-    }
-  }, [openAuthModal]);
 
   useEffect(() => {
     if (activeAccount?.address) {
@@ -272,7 +261,6 @@ export const MultiChainProvider = ({ children }: { children: ReactNode }) => {
         walletAddress,
         balance: selectedChain == "solana" ? solanaBalance : balance,
         connectSolana,
-        connectEthereum,
         disconnectWallet,
         isModalOpen,
         setIsModalOpen,
