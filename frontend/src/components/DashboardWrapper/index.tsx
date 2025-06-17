695929e534da9049194608520437ceecd5ed1c62
olha_sydorchuk <157120293+Oolha@users.noreply.github.com>
2025-06-17 14:53:13 +0200

    feat/AM-131/add profit chart

--- frontend/src/components/DashboardWrapper/index.tsx
+++ frontend/src/components/DashboardWrapper/index.tsx
@@ -3,6 +3,7 @@ import { useVaultData } from "@/hooks/useVaultData";
 import ProfileInfo from "./components/ProfileInfo";
 import PortfolioTabs from "./components/Tabs";
 import TopTokens from "./components/TopTokens";
+import ProfitChart from "./components/ProfitChart";
 
 const DashboardWrapper = () => {
   const { loading, vaults, vaultAPYs, userVaultBalances, vaultTotalAssets } =
@@ -10,16 +11,23 @@ const DashboardWrapper = () => {
 
   return (
     <div className="font-gotham">
-      <div className="text-white text-[40px] font-bold mb-8">
-        <h2>Dashboard</h2>
-      </div>
+      <div className="flex flex-row justify-between items-stretch gap-8">
+        <div className="flex flex-col min-h-[323px]">
+          <div className="text-white text-[40px] font-bold mb-8">
+            <h2>Dashboard</h2>
+          </div>
 
-      <div className="mb-[45px]">
-        <ProfileInfo />
-      </div>
+          <div className="mb-[45px]">
+            <ProfileInfo />
+          </div>
 
-      <div>
-        <TopTokens />
+          <div>
+            <TopTokens />
+          </div>
+        </div>
+        <div className="w-full max-w-[443px] flex pt-6">
+          <ProfitChart className="w-full h-full" />
+        </div>
       </div>
 
       <div className="mt-[82px]">
