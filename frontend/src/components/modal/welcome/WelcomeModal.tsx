972f4ac57818d4341b26d50cfb00e15dc12fde23
olha_sydorchuk <157120293+Oolha@users.noreply.github.com>
2025-06-19 12:15:17 +0200

    feat/AM-166/remove scroll on welcome page

--- frontend/src/components/modal/welcome/WelcomeModal.tsx
+++ frontend/src/components/modal/welcome/WelcomeModal.tsx
@@ -5,10 +5,23 @@ import WelcomeContainer from "@/containers/WelcomeContainer";
 
 import { AnimatePresence, motion } from "framer-motion";
 import GlowIcon from "@/components/svg/GlowIcon";
+import { useEffect } from "react";
 
 const WelcomeModal = () => {
   const { step } = useAuthStore();
   const isVisible = step === "success";
+
+  useEffect(() => {
+    if (isVisible) {
+      const originalStyle = window.getComputedStyle(document.body).overflow;
+
+      document.body.style.overflow = "hidden";
+
+      return () => {
+        document.body.style.overflow = originalStyle;
+      };
+    }
+  }, [isVisible]);
 
   return (
     <AnimatePresence>
@@ -19,7 +32,7 @@ const WelcomeModal = () => {
           animate={{ opacity: 1 }}
           exit={{ opacity: 0 }}
           transition={{ duration: 0.3 }}
-          className="fixed inset-0 z-50 bg-[#0C1015] text-white font-gotham"
+          className="fixed inset-0 z-50 bg-[#0C1015] text-white font-gotham overflow-hidden"
         >
           <GlowIcon position="top-right" />
           <GlowIcon position="bottom-left" />
@@ -28,8 +41,7 @@ const WelcomeModal = () => {
             animate={{ opacity: 1, y: 0, scale: 1 }}
             exit={{ opacity: 0, y: 40, scale: 0.98 }}
             transition={{ duration: 0.4, ease: "easeOut" }}
-            className="flex min-h-screen px-6 py-10
-             items-start justify-center pt-[130px] md:items-center md:pt-10"
+            className="flex min-h-screen px-6 py-10 items-start justify-center pt-[130px] md:items-center md:pt-10 overflow-y-auto"
           >
             <div className="w-full max-w-[1200px]">
               <WelcomeContainer />
