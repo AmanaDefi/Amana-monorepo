7beec731b24ee2edbae1b5f0bbc6e651f380b763
olha_sydorchuk <157120293+Oolha@users.noreply.github.com>
2025-06-23 17:16:26 +0200

    feat/AM-177/update chains dropdowns

--- frontend/src/components/DropdownChainsList.tsx
+++ frontend/src/components/DropdownChainsList.tsx
@@ -42,7 +42,7 @@ export const DropdownChainsList = ({
             width,
             top: !needReset ? 65 : isIconButton ? 30 : 40,
           }}
-          className={`z-10 ${minWidth && "xl:min-w-[526px] md:!w-[220px] !w-[90vw]"} rounded-2xl absolute top-14 flex flex-col gap-2 bg-[#14171F] shadow-[0_4px_6px_0_rgba(0,0,0,0.15)] p-6 ${alignment === "right" ? "md:right-0 -left-[160px] md:left-auto" : "left-0"}`}
+          className={`dropdown-scrollbar z-10 ${minWidth && "xl:min-w-[263px] md:!w-[220px] !w-[90vw]"} rounded-2xl absolute top-14 flex flex-col gap-2 bg-[#14171F] shadow-[0_4px_6px_0_rgba(0,0,0,0.15)] p-2 md:p-4 ${alignment === "right" ? "md:right-0 -left-[160px] md:left-auto" : "left-0"} max-h-[260px] overflow-y-auto`}
           role="menu"
         >
           {isIconButton && (
--- frontend/src/components/VaultsDetailsWrapper/components/WithdrawalNotice.tsx
+++ frontend/src/components/VaultsDetailsWrapper/components/WithdrawalNotice.tsx
@@ -1,7 +1,7 @@
 const WithdrawalNotice = ({}) => {
   return (
     <div className="bg-transparent md:bg-[#161C27] py-0 md:py-[15px] px-0 md:px-[17px] text-[12px] rounded-lg mt-6">
-      <p className="max-w-[349px] text-[#CFD1D4]">
+      <p className="w-full lg:max-w-[349px] text-[#CFD1D4]">
         ⚠️ Notice: This vault uses a 7-day withdrawal request.After you submit an
         Unstake Request, you must wait 7 days before you cam claim your
         funds.
