52c5b3acb48fb7f26a98966185a43aa8d40fb091
olha_sydorchuk <157120293+Oolha@users.noreply.github.com>
2025-06-23 13:52:05 +0200

    feat/AM-177/add swiper to mobile onboarding page

--- frontend/src/components/modal/onboarding/OnboardingModal.tsx
+++ frontend/src/components/modal/onboarding/OnboardingModal.tsx
@@ -23,7 +23,6 @@ const OnboardingModal = () => {
     };
   }, [isOpen]);
 
-
   return (
     <AnimatePresence>
       {isOpen && (
@@ -38,7 +37,7 @@ const OnboardingModal = () => {
           <div className="absolute bottom-0 left-0 w-[300px] h-[300px] blur-[160px] bg-[#3E73C4] opacity-20 pointer-events-none overflow-hidden" />
 
           <div className="relative min-h-screen flex flex-col items-center px-4 sm:px-10 pt-[20px] sm:pt-[34px] pb-12 sm:pb-16">
-            <div className="w-full max-w-[1560px] flex justify-start">
+            <div className="w-full max-w-[1560px] hidden md:flex justify-start">
               <BackButton onClick={closeAll} />
             </div>
             <OnboardingContainer />
