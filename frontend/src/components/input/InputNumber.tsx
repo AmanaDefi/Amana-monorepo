9f4c13325e6d8a75948ada0f21c684593b6014ed
olha_sydorchuk <157120293+Oolha@users.noreply.github.com>
2025-06-26 15:12:05 +0200

    feat/AM-58/create useNumberInput hook for input number refactoring

--- frontend/src/components/input/InputNumber.tsx
+++ frontend/src/components/input/InputNumber.tsx
@@ -1,5 +1,6 @@
-import React, { HTMLProps, useState, useRef } from "react";
+import React, { HTMLProps } from "react";
 import clsx from "clsx";
+import { useNumberInput } from "@/hooks/useNumberInput";
 
 export type InputNumberProps = {
   value: string;
@@ -14,80 +15,12 @@ export default function InputNumber({
   className,
   ...props
 }: InputNumberProps): JSX.Element {
-  const [isFocused, setIsFocused] = useState(false);
-  const [internalValue, setInternalValue] = useState("");
-  const inputRef = useRef<HTMLInputElement>(null);
-
-  const isZeroValue = (val: string): boolean => {
-    return val === "0" || val === "0.00" || val === "0." || val === "";
-  };
-
-  const handleFocus = (e: React.FocusEvent<HTMLInputElement>) => {
-    setIsFocused(true);
-
-    if (isZeroValue(value)) {
-      setInternalValue("");
-    } else {
-      setInternalValue(value);
-    }
-
-    onFocus?.(e);
-  };
-
-  const handleBlur = (e: React.FocusEvent<HTMLInputElement>) => {
-    setIsFocused(false);
-
-    if (internalValue === "" || internalValue === "0.") {
-      setInternalValue("0.00");
-      const syntheticEvent = {
-        ...e,
-        currentTarget: {
-          ...e.currentTarget,
-          value: "0.00",
-        },
-      } as React.ChangeEvent<HTMLInputElement>;
-
-      onChange(syntheticEvent);
-    } else {
-      const syntheticEvent = {
-        ...e,
-        currentTarget: {
-          ...e.currentTarget,
-          value: internalValue,
-        },
-      } as React.ChangeEvent<HTMLInputElement>;
-
-      onChange(syntheticEvent);
-    }
-
-    onBlur?.(e);
-  };
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const inputValue = e.currentTarget.value;
-
-    if (isFocused) {
-      setInternalValue(inputValue);
-
-      if (inputValue === "" || inputValue === "0.") {
-        return;
-      }
-
-      if (!isNaN(Number(inputValue)) && inputValue !== "") {
-        onChange(e);
-      }
-    } else {
-      onChange(e);
-    }
-  };
-
-  const getDisplayValue = (): string => {
-    if (isFocused) {
-      return internalValue;
-    } else {
-      return value;
-    }
-  };
+  const { inputRef, displayValue, handlers } = useNumberInput({
+    value,
+    onChange,
+    onFocus,
+    onBlur,
+  });
 
   return (
     <input
@@ -103,10 +36,8 @@ export default function InputNumber({
       type="text"
       pattern="^[0-9]*[.,]?[0-9]*$"
       spellCheck="false"
-      value={getDisplayValue()}
-      onChange={handleChange}
-      onFocus={handleFocus}
-      onBlur={handleBlur}
+      value={displayValue}
+      {...handlers}
     />
   );
 }
