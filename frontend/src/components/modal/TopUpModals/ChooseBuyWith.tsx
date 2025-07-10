ffd5185e2d07795d5279f116fb9edb4c2a9c1c5b
olha_sydorchuk <157120293+Oolha@users.noreply.github.com>
2025-07-10 09:29:57 +0200

    fix/AM-265/add coming soon for buy crypto button

--- frontend/src/components/modal/TopUpModals/ChooseBuyWith.tsx
+++ frontend/src/components/modal/TopUpModals/ChooseBuyWith.tsx
@@ -47,7 +47,7 @@ export const ChooseBuyWith = () => {
           </h2>
           <div className="flex flex-col gap-4">
             <ModalButton
-              text="Using Card"
+              text="Coming soon"
               className="w-[316px] md:w-[484px]"
               label="Buy crypto"
               withArrow
