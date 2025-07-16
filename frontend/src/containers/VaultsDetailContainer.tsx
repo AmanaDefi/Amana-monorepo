f498656791cbeef3db4c32aaead108148365265b
olha_sydorchuk <157120293+Oolha@users.noreply.github.com>
2025-07-16 15:30:07 +0200

    fix/add loader for apy7d value on details page

--- frontend/src/containers/VaultsDetailContainer.tsx
+++ frontend/src/containers/VaultsDetailContainer.tsx
@@ -616,7 +616,10 @@ const VaultsDetailContainer: React.FC<{
                     (a) => a.vaultId === vaultID.toString(),
                   )}
                   totalAssets={vaultTotalAsset}
-                  isLoading={loading || !vaultAPYs.length}
+                  isLoading={
+                    loading ||
+                    !vaultAPYs.find((a) => a.vaultId === vaultID.toString())
+                  }
                   isDeposit={isDeposit}
                   isReward={true}
                 />
@@ -630,6 +633,10 @@ const VaultsDetailContainer: React.FC<{
                     )}
                     totalAssets={vaultTotalAsset}
                     titleColor="#535E73"
+                    isLoading={
+                      loading ||
+                      !vaultAPYs.find((a) => a.vaultId === vaultID.toString())
+                    }
                     isDeposit={isDeposit}
                     isReward={true}
                   />
