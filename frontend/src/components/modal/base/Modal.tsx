9d5110d274e67c52a433c42287ffa538fd5e33b8
olha_sydorchuk <157120293+Oolha@users.noreply.github.com>
2025-07-29 12:25:10 +0200

    fix/padding for mobile footer

--- frontend/src/components/modal/base/Modal.tsx
+++ frontend/src/components/modal/base/Modal.tsx
@@ -49,8 +49,8 @@ export const Modal = ({
                 <GlowIcon position="bottom-mobile" />
               </div>
             </motion.div>
+            <div className="fixed inset-0 flex flex-col md:items-center md:justify-center justify-between items-center pt-10 p-4 pb-2 h-[100dvh] md:h-[95vh]">
 
-            <div className="fixed inset-0 flex flex-col md:items-center md:justify-center justify-between items-center pt-10 p-4 pb-10 h-[100dvh] md:h-[100vh]">
               <DialogPanel as={Fragment}>
                 <motion.div
                   key="modal"
