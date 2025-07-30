3cbeb326e69fbd462ae1fb718b5d4b643fbff200
olha_sydorchuk <157120293+Oolha@users.noreply.github.com>
2025-07-30 15:32:25 +0200

    fix/reset page to 1 when switching vault views

--- frontend/src/components/VaultsWrapper/index.tsx
+++ frontend/src/components/VaultsWrapper/index.tsx
@@ -138,6 +138,12 @@ const VaultsGrid: React.FC<VaultsGridProps> = ({
   const itemsPerPage = useLayoutStore((state) => state.itemsPerPage);
 
   useEffect(() => {
+    if (onPageChange) {
+      onPageChange(1);
+    }
+  }, [isShownMyVaults]);
+
+  useEffect(() => {
     setSortBy(externalSortBy);
     setSortOrder(externalSortOrder);
   }, [externalSortBy, externalSortOrder]);
